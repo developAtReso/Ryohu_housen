c45f3a2d62ff17eb9e824042fe96412738e551ca
Takahiro Suga <mustplus3692@outlook.jp>
2020-02-03 14:44:05 +0900

    Laravel_mail

--- app/Http/Controllers/SugaController.php
+++ app/Http/Controllers/SugaController.php
@@ -7,11 +7,14 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\OrderShipped;
 
 class SugaController extends Controller
 {
     public function index($url = 'sugatest')
     {
+        Mail::to('test@gmail.com')->send(new OrderShipped());
         $sugadb = DB::table('sugatest')->get();
         return view($url, ['name' => 'ssssuga',
                            'sugadb' => $sugadb,
