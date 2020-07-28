e9cb8b9a5ad0266352bcc91bcbfe079a46c400ab
nttr-t-suga <51395355+nttr-t-suga@users.noreply.github.com>
2020-07-28 11:33:54 +0900

    test (#2)
    Co-authored-by: Takahiro Suga <mustplus3692@outlook.jp>

--- app/Http/Controllers/SugaController.php
+++ app/Http/Controllers/SugaController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;
